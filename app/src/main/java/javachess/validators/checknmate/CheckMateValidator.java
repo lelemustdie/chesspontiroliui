f21042646d139d1f5ff67493c8fe1f6239e1ad9f
lourdes pontiroli <pontiroli.lourdes@gmail.com>
2023-11-21 17:37:52 -0300

    rollback CheckMateValidator

--- app/src/main/java/javachess/validators/checknmate/CheckMateValidator.java
+++ app/src/main/java/javachess/validators/checknmate/CheckMateValidator.java
@@ -2,13 +2,13 @@ package javachess.validators.checknmate;
 
 import common.Board;
 import common.Coordinate;
-import common.enums.Color;
 import common.Movement;
 import common.Piece;
+import common.enums.Color;
 import common.validators.Validator;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class CheckMateValidator implements Validator {
@@ -17,39 +17,27 @@ public class CheckMateValidator implements Validator {
     public CheckMateValidator(Validator checkValidator) {
         this.checkValidator = checkValidator;
     }
-
     @Override
-    public boolean isValid(List<Board> history, Movement movement) {
+    public boolean isValid(List<Board> history, Movement movement) { //chequea si todos los movimientos q puedo hacer con mi rey me dejan en check, es checkmate
         Board previousBoard = history.get(history.size() - 2);
-        Color currentPlayer = determineCurrentPlayer(previousBoard, movement);
+        Color currentPlayer = previousBoard.getPieces().get(movement.getOrigin()).getColor() == Color.WHITE ? Color.BLACK : Color.WHITE;
         Board currentBoard = history.get(history.size() - 1);
         Map<Coordinate, Piece> pieces = currentBoard.getPieces();
 
-        for (int currentRow = 1; currentRow <= currentBoard.getRows(); currentRow++) {
-            for (int currentColumn = 1; currentColumn <= currentBoard.getColumns(); currentColumn++) {
-                Coordinate currentDestination = new Coordinate(currentColumn, currentRow);
-                checkMovesAndValidate(history, currentPlayer, currentBoard, pieces, currentDestination);
+        for (int row = 1; row <= currentBoard.getRows(); row ++){ //itera cada posicion del tablero, si se puede mover ahi
+            for (int column = 1; column <= currentBoard.getColumns(); column++){
+                Coordinate currentDestination = new Coordinate(column, row);
+                for(Map.Entry<Coordinate, Piece> piece : pieces.entrySet()){
+                    Movement move = new Movement(piece.getKey(), currentDestination);
+                    if (piece.getValue().getColor() == currentPlayer && piece.getValue().getValidator().isValid(history, move)) {
+                        Board newBoard = currentBoard.movePiece(move);
+                        List<Board> newHistory = new ArrayList<>(history);
+                        newHistory.add(newBoard);
+                        if (checkValidator.isValid(newHistory, move)) return false;
+                    }
+                }
             }
         }
         return true;
-    }
-
-    private Color determineCurrentPlayer(Board previousBoard, Movement movement) {
-        return previousBoard.getPieces().get(movement.getOrigin()).getColor() == Color.WHITE ? Color.BLACK : Color.WHITE;
-    }
-
-    private boolean checkMovesAndValidate(List<Board> history, Color currentPlayer, Board currentBoard,
-                                       Map<Coordinate, Piece> pieces, Coordinate currentDestination) {
-        for (Map.Entry<Coordinate, Piece> piece : pieces.entrySet()) {
-            Movement move = new Movement(piece.getKey(), currentDestination);
-            if (piece.getValue().getColor() == currentPlayer && piece.getValue().getValidator().isValid(history, move)) {
-                Board newBoard = currentBoard.movePiece(move);
-                List<Board> newHistory = new ArrayList<>(history);
-                newHistory.add(newBoard);
-                if (checkValidator.isValid(newHistory, move)) {
-                    return false;
-                }
-            }
-        } return true;
     }
 }
